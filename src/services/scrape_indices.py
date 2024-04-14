0c705a756e00d48d8bdde800d48b16b6050a2cd0
harvey <harveytseng2@gmail.com>
2024-04-13 21:21:33 -0400

    Optimize scrape_indices.py

--- src/services/scrape_indices.py
+++ src/services/scrape_indices.py
@@ -1,39 +1,50 @@
+import re
+from aiohttp import ClientSession, TCPConnector
+from bs4 import BeautifulSoup, SoupStrainer
 from src.constants import headers
 from decimal import Decimal
-from bs4 import BeautifulSoup
 from fastapi.responses import JSONResponse
-import requests
 from src import schemas
+import asyncio
+
+# Compile regular expressions
+re_decimal = re.compile(r'\d+')
+
+
+async def fetch_and_parse(session, url, semaphore):
+    async with semaphore, session.get(url, headers=headers) as response:
+        html = await response.text()
+        return await parse_html(html)
+
+
+async def parse_html(html):
+    parse_only = SoupStrainer('table', {'id': 'indice_table_1'})
+    soup = BeautifulSoup(html, 'lxml', parse_only=parse_only)  # Use 'lxml' as the parser
+    table = soup.find('table', {'id': 'indice_table_1'})
+    indices = []
+    if table:
+        rows = table.find_all('tr')
+        for row in rows:
+            cells = row.find_all('td')
+            if len(cells) > 5:
+                index_data = schemas.Index(
+                    name=cells[1].text,
+                    value=Decimal(re_decimal.search(cells[2].text).group()),
+                    change=cells[5].text,
+                    percent_change=cells[6].text,
+                )
+                indices.append(index_data)
+    return indices
 
 
 async def scrape_indices():
-    url = 'https://www.investing.com/indices/americas-indices'
-    response = requests.get(url, headers=headers)
+    urls = ['https://www.investing.com/indices/americas-indices']
+    semaphore = asyncio.Semaphore(25)  # Limit to 10 concurrent requests
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        # Find the table element by its ID
-        table = soup.find('table', {'id': 'indice_table_1'})
-        indices = []
-        # Check if the table is found
-        if table:
-            # Extract table rows
-            rows = table.find_all('tr')
-
-            # Iterate through rows
-            for row in rows:
-                # Extract table data from each row
-                cells = row.find_all('td')
-                if len(cells) > 5:  # Ensure there are enough cells
-                    index_data = schemas.Index(
-                        name=cells[1].text,
-                        value=Decimal(cells[2].text.replace(',', '')),  # Convert string to Decimal
-                        change=cells[5].text,
-                        percent_change=cells[6].text,
-                    )
-                    indices.append(index_data)
-            return indices
-        else:
-            return JSONResponse(status_code=500, content={"message": "Internal server error"})
-    else:
-        return JSONResponse(status_code=500, content={"message": "Failed to fetch data from the URL"})
+    try:
+        async with ClientSession(connector=TCPConnector(limit=25)) as session:
+            tasks = [fetch_and_parse(session, url, semaphore) for url in urls]
+            all_indices = await asyncio.gather(*tasks)
+            return [index for indices in all_indices for index in indices]
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"message": str(e)})
