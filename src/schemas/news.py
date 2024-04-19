3eb2df687bf79912ff438d42f7665352ba360457
harvey <harveytseng2@gmail.com>
2024-04-19 11:53:52 -0400

    Update quote and news model to include more information.

--- src/schemas/news.py
+++ src/schemas/news.py
@@ -5,4 +5,5 @@ class News(BaseModel):
     title: str = Field(..., example="New iPhone released!", description="Title of the news article")
     link: str = Field(..., example="https://www.example.com", description="Link to the news article")
     source: str = Field(..., example="CNN", description="Source of the news article")
+    img: str = Field(..., example="https://www.example.com/image.jpg", description="Image of the news article")
     time: str = Field(..., example="1 day ago", description="Time relative to current time when the news was published")
--- src/schemas/quote.py
+++ src/schemas/quote.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel, Field
 from decimal import Decimal
-from .stocks import Stock
+
+from typing_extensions import Optional
+
+from .stock import Stock
 from .news import News
 
 
@@ -8,7 +11,7 @@ class Quote(BaseModel):
     symbol: str = Field(..., example="AAPL", description="Stock symbol")
     name: str = Field(..., example="Apple Inc.", description="Company name")
     price: Decimal = Field(..., example=145.00, description="Last traded price of the stock")
-    after_hours_price: Decimal = Field(..., example=145.50, description="After hours price of the stock")
+    after_hours_price: Optional[Decimal] = Field(None, example=145.50, description="After hours price of the stock")
     change: str = Field(..., example="+1.00", description="Change in the stock price")
     percent_change: str = Field(..., example="+0.69%", description="Percentage change in the stock price")
     open: Decimal = Field(..., example=144.00, description="Opening price of the stock")
@@ -20,16 +23,17 @@ class Quote(BaseModel):
     avg_volume: int = Field(..., example=2000000, description="Average volume of the stock")
     market_cap: str = Field(..., example="2.5T", description="Market capitalization of the stock")
     beta: Decimal = Field(..., example=1.23, description="Beta of the stock")
-    pe: Decimal = Field(..., example=30.00, description="Price to earnings ratio of the stock")
-    eps: Decimal = Field(..., example=4.50, description="Earnings per share of the stock")
-    dividend: Decimal = Field(..., example=0.82, description="Dividend yield of the stock")
-    earnings_date: str = Field(..., example="Apr 23, 2024", description="Next earnings date of the stock")
-    about: str = Field(...,
+    pe: Optional[Decimal] = Field(None, example=30.00, description="Price to earnings ratio of the stock")
+    eps: Optional[Decimal] = Field(None, example=4.50, description="Earnings per share of the stock")
+    dividend: Optional[Decimal] = Field(None, example=0.82, description="Dividend yield of the stock")
+    ex_dividend: Optional[str] = Field(None, example="Feb 5, 2024", description="Ex-dividend date of the stock")
+    earnings_date: Optional[str] = Field(None, example="Apr 23, 2024", description="Next earnings date of the stock")
+    sector: str = Field(..., example="Technology", description="Sector of the company")
+    industry: str = Field(..., example="Consumer Electronics", description="Industry of the company")
+    about: Optional[str] = Field(None,
                        example="Apple Inc. designs, manufactures, and markets smartphones, personal computers, "
                                "tablets, wearables, and accessories worldwide.",
                        description="About the company")
-    sector: str = Field(..., example="Technology", description="Sector of the company")
-    industry: str = Field(..., example="Consumer Electronics", description="Industry of the company")
     similar_stocks: list[Stock] = Field(...,
                                         example=[{"symbol": "MSFT", "name": "Microsoft Corporation", "price": 300.00}],
                                         description="List of similar stocks")
