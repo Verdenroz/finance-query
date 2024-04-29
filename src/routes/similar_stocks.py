274ced55b7a4f241c09225e59bc411e5a228fb69
harvey <harveytseng2@gmail.com>
2024-04-29 16:57:15 -0400

    Handle similar equities for etfs in scrape_similar_stocks

---
+++ src/routes/similar_stocks.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Security, HTTPException, Query
+from fastapi.security import APIKeyHeader
+from typing_extensions import List
+from src.schemas import Stock
+from src.services import scrape_similar_stocks
+from src.utils import cache
+
+router = APIRouter()
+
+
+@router.get("/v1/similar-stocks/",
+            summary="Returns summary quote data of a single stock",
+            description="Get relevant stock information for a single stock. "
+                        "Invalid API keys are limited to 5 requests per minute.",
+            response_model=List[Stock],
+            dependencies=[Security(APIKeyHeader(name="x-api-key", auto_error=False))],
+            responses={400: {"description": "Symbol parameter is required"}})
+@cache(600)
+async def get_similar_stocks(
+        symbol: str = Query(..., title="Symbol", description="Stock to find similar stocks around")):
+    if not symbol:
+        raise HTTPException(status_code=400, detail="Symbol parameter is required")
+    return await scrape_similar_stocks(symbol)
