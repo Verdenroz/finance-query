fdede737b3b96103058bcb831e482aad82cfe051
harvey <harveytseng2@gmail.com>
2024-05-04 21:34:58 -0400

    Add function to get a list of historical YahooQuery Quotes

--- src/services/get_historical.py
+++ src/services/get_historical.py
@@ -1,7 +1,13 @@
+from datetime import datetime, time
+from decimal import Decimal
+
 import pandas as pd
+from fastapi import HTTPException
+from requests.exceptions import RetryError
+from stock_indicators.indicators.common.quote import Quote
+from typing_extensions import List
 from yahooquery import Ticker
 
-from decimal import Decimal
 from src.schemas import HistoricalData, TimeSeries
 from src.schemas.time_series import Interval, TimePeriod
 
@@ -48,4 +54,27 @@ async def get_historical(symbol: str, time: TimePeriod, interval: Interval):
         else:
             raise HTTPException(status_code=500, detail="Internal server error")
 
-    return TimeSeries(history=data_dict)
+
+async def get_historical_quotes(symbol: str, timePeriod: TimePeriod, interval: Interval) -> List[Quote]:
+    try:
+        stock = Ticker(symbol, asynchronous=True, retry=3, status_forcelist=[404, 429, 500, 502, 503, 504])
+        data = stock.history(period=timePeriod.value, interval=interval.value, adj_ohlc=True)
+        data = data.sort_index(ascending=False)
+        quotes = []
+        for _, row in data.iterrows():
+            if row.name[1] is not None:
+                date = row.name[1]
+                if not isinstance(date, datetime):
+                    date = datetime.combine(date, time())  # Convert date to datetime.datetime
+                if date is not None:
+                    quotes.append(
+                        Quote(date=date, open=row['open'], high=row['high'], low=row['low'], close=row['close'],
+                              volume=row['volume'])
+                    )
+        return quotes
+
+    except RetryError as e:
+        if '404' in str(e):
+            raise HTTPException(status_code=404, detail="Stock not found")
+        else:
+            raise HTTPException(status_code=500, detail="Internal server error")
