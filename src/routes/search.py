35283e4d7a8c08a92a4f69e38368eccfc601db42
harvey <harveytseng2@gmail.com>
2024-05-11 21:24:59 -0400

    Update route params and error handling

--- src/routes/search.py
+++ src/routes/search.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
-from typing_extensions import Optional
+from fastapi import APIRouter, Security, HTTPException
+from fastapi.security import APIKeyHeader
+from typing_extensions import Optional, List
 
+from src.schemas import SearchResult
 from src.services import get_search
 from src.services.get_search import Type
 
@@ -10,6 +12,11 @@ router = APIRouter()
 @router.get("/search",
             summary="Search for a stock",
             description="Search for a stock by name or symbol.",
-            )
+            response_model=List[SearchResult],
+            response_description="List of search results",
+            dependencies=[Security(APIKeyHeader(name="x-api-key", auto_error=False))],
+            responses={400: {"description": "Query parameter is required"}})
 async def search(query: str, type: Optional[Type] = None):
+    if not query:
+        raise HTTPException(status_code=400, detail="Query parameter is required")
     return await get_search(query, type)
