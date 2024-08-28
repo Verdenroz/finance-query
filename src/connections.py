f41c287abbc250155a2aa2dd927ee8468f42b615
harvey <harveytseng2@gmail.com>
2024-08-28 19:54:00 -0400

    creates connection manager for redis pubsub in sockets

---
+++ src/connections.py
@@ -0,0 +1,83 @@
+import asyncio
+from typing import Dict, List
+
+from orjson import orjson
+from starlette.websockets import WebSocket, WebSocketDisconnect
+
+from src.utils import r
+
+
+class RedisConnectionManager:
+    """
+    Manages active websocket connections and listens to Redis channels to broadcast messages to clients.
+    """
+
+    def __init__(self):
+        self.active_connections: Dict[str, List[WebSocket]] = {}
+        self.pubsub = r.pubsub()
+
+    async def connect(self, websocket: WebSocket, channel: str):
+        """
+        Connects a websocket to a channel and listens for messages.
+        :param websocket: the client websocket connection
+        :param channel: the channel to subscribe to
+        :return:
+        """
+        if channel not in self.active_connections:
+            self.active_connections[channel] = []
+
+        self.active_connections[channel].append(websocket)
+
+        asyncio.create_task(self._listen_to_channel(channel))
+
+    async def disconnect(self, websocket: WebSocket, channel: str):
+        """
+        Disconnects a websocket from a channel.
+        :param websocket: the client websocket connection to disconnect
+        :param channel: the channel to disconnect from
+        :return:
+        """
+        self.active_connections[channel].remove(websocket)
+        if not self.active_connections[channel]:
+            del self.active_connections[channel]
+            await self.pubsub.unsubscribe(channel)
+
+    async def _listen_to_channel(self, channel: str):
+        """
+        Listens to a Redis channel and broadcasts messages to clients.
+        :param channel: the channel to subscribe to with Redis PubSub
+        :return:
+        """
+        await self.pubsub.subscribe(channel)
+        while True:
+            message = await self.pubsub.get_message()
+            if message and message['type'] == 'message':
+                data = orjson.loads(message['data'])
+                await self._broadcast(channel, data)
+
+    async def _broadcast(self, channel: str, message: dict):
+        """
+        Broadcasts a message to all clients connected to a channel.
+        :param channel: the channel to broadcast to
+        :param message: the message json to broadcast
+        :return:
+        """
+        if channel in self.active_connections:
+            disconnected_clients = []
+            for connection in self.active_connections[channel]:
+                try:
+                    await connection.send_json(message)
+                except WebSocketDisconnect:
+                    disconnected_clients.append(connection)
+            for client in disconnected_clients:
+                await self.disconnect(client, channel)
+
+    @staticmethod
+    async def publish(message: dict, channel: str):
+        """
+        Publishes a message to a Redis channel.
+        :param message: the json message to publish
+        :param channel: the channel to publish to
+        :return:
+        """
+        await r.publish(channel, orjson.dumps(message))
--- src/routes/sockets.py
+++ src/routes/sockets.py
@@ -1,8 +1,10 @@
 import asyncio
 
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from fastapi import APIRouter
+from starlette.websockets import WebSocket, WebSocketDisconnect
 
-from src.services import scrape_quotes, scrape_simple_quotes, scrape_similar_stocks, scrape_actives, \
+from src.connections import RedisConnectionManager
+from src.services import scrape_quotes, scrape_similar_stocks, scrape_actives, \
     scrape_news_for_quote, scrape_losers, scrape_gainers
 from src.services.get_sectors import get_sector_for_symbol, get_sectors
 
@@ -10,13 +12,19 @@ router = APIRouter()
 
 
 @router.websocket("/profile/{symbol}/ws")
-async def websocket_profile(
-        websocket: WebSocket,
-        symbol: str
-):
+async def websocket_profile(websocket: WebSocket, symbol: str):
+    connection_manager = RedisConnectionManager()
     await websocket.accept()
+
+    channel = f"profile:{symbol}"  # Expected channel name, but can be anything from client
+
     try:
         while True:
+            channel = await websocket.receive_text()  # Raises WebSocketDisconnect if client disconnects
+
+            if websocket not in connection_manager.active_connections.get(channel, []):
+                await connection_manager.connect(websocket, channel)
+
             quotes_task = scrape_quotes([symbol])
             similar_stocks_task = scrape_similar_stocks(symbol)
             sector_performance_task = get_sector_for_symbol(symbol)
@@ -38,11 +46,9 @@ async def websocket_profile(
                 "performance": sector_performance,
                 "news": news
             }
-
-            await websocket.send_json(result)
-            await asyncio.sleep(5)
+            await connection_manager.publish(result, channel)
     except WebSocketDisconnect:
-        await websocket.close()
+        await connection_manager.disconnect(websocket, channel)
 
 
 @router.websocket("/ws/quotes")
@@ -62,16 +68,26 @@ async def websocket_quotes(websocket: WebSocket):
 
 @router.websocket("/ws/market")
 async def websocket_market(websocket: WebSocket):
+    connection_manager: RedisConnectionManager = RedisConnectionManager()
+
     await websocket.accept()
+    channel = "market"  # Expected channel name, but can be anything from client
+
     try:
         while True:
+            channel = await websocket.receive_text()  # Raises WebSocketDisconnect if client disconnects
+
+            if websocket not in connection_manager.active_connections.get(channel, []):
+                await connection_manager.connect(websocket, channel)
+
             actives_task = scrape_actives()
             gainers_task = scrape_gainers()
             losers_task = scrape_losers()
             sectors_task = get_sectors()
 
             actives, gainers, losers, sectors = await asyncio.gather(
-                actives_task, gainers_task, losers_task, sectors_task)
+                actives_task, gainers_task, losers_task, sectors_task
+            )
 
             actives = [active if isinstance(active, dict) else active.dict() for active in actives]
             gainers = [gainer if isinstance(gainer, dict) else gainer.dict() for gainer in gainers]
@@ -84,8 +100,7 @@ async def websocket_market(websocket: WebSocket):
                 "losers": losers,
                 "sectors": sectors
             }
-            await websocket.send_json(result)
-            await asyncio.sleep(10)
 
+            await connection_manager.publish(result, channel)
     except WebSocketDisconnect:
-        await websocket.close()
+        await connection_manager.disconnect(websocket, channel)
