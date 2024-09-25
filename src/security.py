ad140901b188fc8372b16fb4a2987aa816121813
harvey <harveytseng2@gmail.com>
2024-09-25 19:54:27 -0400

    creates functions to validate and enforce rate limit

--- src/security.py
+++ src/security.py
@@ -11,6 +11,35 @@ ADMIN_API_KEY = os.getenv("ADMIN_API_KEY")
 RATE_LIMIT = 500  # requests per day
 
 
+async def validate_api_key(api_key: str) -> bool:
+    if api_key not in {DEMO_API_KEY, ADMIN_API_KEY}:
+        return False
+    return True
+
+
+async def enforce_rate_limit(api_key: str) -> bool:
+    """
+    Enforce rate limiting for the demo API key
+    :param api_key: should be FinanceQueryDemoAWSHT, but can be any string
+
+    :return: True if the rate limit has not been exceeded, False otherwise
+    """
+    if api_key == DEMO_API_KEY:
+        key = f"rate_limit:{api_key}"
+        count = await r.get(key)
+        if count is None:
+            # Set the key to 1 and expire it in 24 hours if it doesn't exist
+            await r.set(key, 1, ex=86400)
+        elif int(count) >= RATE_LIMIT:
+            # Rate limit exceeded
+            return False
+        else:
+            # Increment the count
+            await r.incr(key)
+
+    return True
+
+
 class RateLimitMiddleware(BaseHTTPMiddleware):
     """
     Middleware to enforce rate limiting on the API
@@ -31,21 +60,16 @@ class RateLimitMiddleware(BaseHTTPMiddleware):
         """
         api_key = request.headers.get("x-api-key")
 
+        if not await validate_api_key(api_key):
+            return JSONResponse({"detail": "Not authenticated"}, status_code=403)
+
         if api_key == DEMO_API_KEY:
+            if not await enforce_rate_limit(api_key):
+                return JSONResponse({"detail": "Rate limit exceeded"}, status_code=429)
+
             key = f"rate_limit:{api_key}"
             count = await r.get(key)
-            if count is None:
-                # Set the key and set the expiration time to 24 hours
-                await r.set(key, 1, ex=86400)
-                count = 0
-            elif int(count) >= RATE_LIMIT:
-                # Rate limit exceeded
-                return JSONResponse({"detail": "Rate limit exceeded"}, status_code=429)
-            else:
-                # Increment the count
-                await r.incr(key)
-
-            remaining = RATE_LIMIT - int(count) - 1
+            remaining = RATE_LIMIT - int(count)
             response = await call_next(request)
             response.headers["X-RateLimit-Limit"] = str(RATE_LIMIT)
             response.headers["X-RateLimit-Remaining"] = str(remaining)
@@ -56,6 +80,3 @@ class RateLimitMiddleware(BaseHTTPMiddleware):
             # No rate limit for admin API key
             response = await call_next(request)
             return response
-        else:
-            # When there is no API key or an invalid API key
-            return JSONResponse({"detail": "Not authenticated"}, status_code=403)
