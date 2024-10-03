c3e14ea2ab3a8e4bc027fbe22fff0071978bcc27
harvey <harveytseng2@gmail.com>
2024-10-03 19:13:35 -0400

    maintains parsing similars for etfs

--- src/services/get_similar_stocks.py
+++ src/services/get_similar_stocks.py
@@ -81,12 +81,12 @@ async def _parse_stocks(stocks_divs, symbol) -> List[SimpleQuote]:
 async def _parse_etfs(etf_divs):
     etfs = []
     for div in etf_divs:
-        symbol_element = div.find("span", class_="symbol yf-ravs5v")
+        symbol_element = div.find("span", class_="symbol yf-1jpysdn")
         if not symbol_element:
             continue
         symbol = symbol_element.text
 
-        name_element = div.find("span", class_="tw-text-sm yf-ravs5v longName")
+        name_element = div.find("span", class_="tw-text-sm yf-1jpysdn longName")
         if not name_element:
             continue
         name = name_element.text
