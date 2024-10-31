23db025c179f0f4a99ffc384a0b27ec0524ed708
harvey <harveytseng2@gmail.com>
2024-10-31 19:32:09 -0400

    refactors scraping market movers to use xpaths

--- src/services/get_movers.py
+++ src/services/get_movers.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 
-from bs4 import BeautifulSoup, SoupStrainer
 from fastapi import HTTPException
+from lxml import etree
 
 from ..redis import cache
 from ..schemas.marketmover import MarketMover
@@ -35,26 +35,43 @@ async def _scrape_movers(url: str) -> list[MarketMover]:
     :raises: HTTPException with status code 500 if an error occurs while scraping
     """
     html = await fetch(url)
-    parse_only = SoupStrainer('tr', attrs={'class': 'row false  yf-1dbt8wv'})
-    soup = BeautifulSoup(html, 'lxml', parse_only=parse_only)
+    tree = etree.HTML(html)
+
+    tbody_xpath = '/html/body/div[2]/main/section/section/section/article/section[1]/div/div[2]/div/table/tbody'
+    row_xpath = './/tr'
+    symbol_xpath = './/td[1]//a/@title'
+    name_xpath = './/td[2]//div/text()'
+    price_xpath = './/td[4]//fin-streamer[@data-field="regularMarketPrice"]/text()'
+    change_xpath = './/td[5]/span/fin-streamer/span/text()'
+    percent_change_xpath = './/td[6]/span/fin-streamer/span/text()'
+
+    tbody_element = tree.xpath(tbody_xpath)[0]
+    rows = tbody_element.xpath(row_xpath)
     movers = []
 
-    for row in soup.find_all('tr'):
-        cells = row.find_all('td', limit=4)
 
-        symbol = cells[0].find('span', class_='symbol').text.strip()
-        name = cells[0].find('span', class_='longName').text.strip()
-        price = cells[1].find('fin-streamer', {'data-field': 'regularMarketPrice'}).text.strip()
-        change = cells[2].find('fin-streamer', {'data-field': 'regularMarketChange'}).text.strip()
-        percent_change = cells[3].find('fin-streamer', {'data-field': 'regularMarketChangePercent'}).text.strip()
-        mover = MarketMover(
-            symbol=symbol,
-            name=name,
-            price=Decimal(price.replace(',', '')),
-            change=change,
-            percent_change=percent_change
-        )
-        movers.append(mover)
+    for row in rows:
+        symbol_elements = row.xpath(symbol_xpath)
+        name_elements = row.xpath(name_xpath)
+        price_elements = row.xpath(price_xpath)
+        change_elements = row.xpath(change_xpath)
+        percent_change_elements = row.xpath(percent_change_xpath)
+
+        if symbol_elements and name_elements and price_elements and change_elements and percent_change_elements:
+            symbol = symbol_elements[0].strip()
+            name = name_elements[0].strip()
+            price = Decimal(price_elements[0].strip().replace(',', ''))
+            change = change_elements[0].strip()
+            percent_change = percent_change_elements[0].strip('()')
+
+            mover = MarketMover(
+                symbol=symbol,
+                name=name,
+                price=price,
+                change=change,
+                percent_change=percent_change
+            )
+            movers.append(mover)
 
     # If no movers are found, raise an HTTPException
     if not movers:
