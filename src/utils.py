42cd4d4b68b866f3baa58cf4a86c2ca5def018b7
harvey <harveytseng2@gmail.com>
2024-11-16 21:07:27 -0500

    replace util is_market_open with market_schedule

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,6 @@
 import os
-from datetime import datetime
 from typing import Optional
 
-import pytz
 from aiohttp import ClientSession
 from dotenv import load_dotenv
 
@@ -12,15 +10,8 @@ from src.session_manager import get_global_session
 load_dotenv()
 
 
-def is_market_open() -> bool:
-    now = datetime.now(pytz.timezone('US/Eastern'))
-    open_time = datetime.now(pytz.timezone('US/Eastern')).replace(hour=9, minute=30, second=0)
-    close_time = datetime.now(pytz.timezone('US/Eastern')).replace(hour=16, minute=0, second=0)
-    # Check if current time is within market hours and it's a weekday
-    return open_time <= now <= close_time and 0 <= now.weekday() < 5
-
-
-async def fetch(url: str,session: Optional[ClientSession] = None, use_proxy: bool = os.getenv('USE_PROXY', 'False') == 'True') -> str:
+async def fetch(url: str, session: Optional[ClientSession] = None,
+                use_proxy: bool = os.getenv('USE_PROXY', 'False') == 'True') -> str:
     session = session or await get_global_session()
     if use_proxy:
         if proxy is None or proxy_auth is None:
