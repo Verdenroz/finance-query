a3e665ab6a68194a12fb0cf0fb5bfec14dc8ddbc
harvey <harveytseng2@gmail.com>
2025-01-19 21:18:56 -0500

    adds open source algolia creds

--- src/services/get_search.py
+++ src/services/get_search.py
@@ -13,8 +13,15 @@ class Type(Enum):
     TRUST = "trust"
 
 
-async def get_search(query: str, type: Optional[Type] = None):
-    client = SearchClient.create(os.environ['ALGOLIA_APP_ID'], os.environ['ALGOLIA_KEY'])
+async def get_search(query: str, type: Optional[Type] = None, hits: Optional[int] = 10):
+    # My personal Algolia credentials are on free tier and are safe to be shared
+    # This is out of trust that the search will not be abused
+    # The API key is search-only and can't be used to modify the data
+    # If you would like your own personal index, email me and I can send you a json of all tradable stocks/etfs/trusts
+    client = SearchClient.create(
+        app_id=os.environ.get('ALGOLIA_APP_ID', "ZTZOECLXBC"),
+        api_key=os.environ.get('ALGOLIA_API_KEY', 'a3882d6ec31c0b1063ede94374616d8a')
+    )
     index = client.init_index("stocks")
 
     # Search parameters
