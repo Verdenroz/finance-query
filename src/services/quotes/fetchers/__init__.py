ab98511e1e93388cd5e05e718b08b0cd2a8bb113
harvey <harveytseng2@gmail.com>
2025-02-05 11:12:19 -0500

    renames fetchers to quote scraper/api

--- src/services/quotes/fetchers/__init__.py
+++ src/services/quotes/fetchers/__init__.py
@@ -1,2 +1,2 @@
-from .yahoo_api import fetch_quotes, fetch_simple_quotes
-from .yahoo_scraper import scrape_quotes, scrape_simple_quotes
+from .quote_api import fetch_quotes, fetch_simple_quotes
+from .quote_scraper import scrape_quotes, scrape_simple_quotes
