e633152724f8fdfe386f06c0b776ff617a8f74ff
harvey <harveytseng2@gmail.com>
2025-02-10 08:31:55 -0500

    adds cython calculations for trend indicators

--- src/services/indicators/core/__init__.py
+++ src/services/indicators/core/__init__.py
@@ -1,3 +1,5 @@
 from .moving_averages import calculate_sma, calculate_ema, calculate_wma, calculate_vwma
 from .oscillators import calculate_rsi, calculate_stoch_rsi, calculate_stoch, calculate_cci
+from .trends import calculate_macd, calculate_adx, calculate_aroon, calculate_bbands, calculate_obv, \
+    calculate_supertrend, calculate_ichimoku
 from .utils import create_indicator_dict, prepare_price_data
