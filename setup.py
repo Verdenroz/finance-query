308ec4b6f78d691ec352efe898704c7e714c8ce6
harvey <harveytseng2@gmail.com>
2025-02-11 21:48:26 -0500

    create extensions from list of modules

--- setup.py
+++ setup.py
@@ -4,34 +4,19 @@ import numpy
 from Cython.Build import cythonize
 from setuptools import setup, Extension
 
-# Define the core directory path
 CORE_DIR = "src/services/indicators/core"
+
+module_names = ["utils", "moving_averages", "oscillators", "trends"]
+
 
 extensions = [
     Extension(
-        "utils",
-        [os.path.join(CORE_DIR, "utils.pyx")],
+        name,
+        [os.path.join(CORE_DIR, f"{name}.pyx")],
         include_dirs=[numpy.get_include()],
-        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
-    ),
-    Extension(
-        "moving_averages",
-        [os.path.join(CORE_DIR, "moving_averages.pyx")],
-        include_dirs=[numpy.get_include()],
-        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
-    ),
-    Extension(
-        "oscillators",
-        [os.path.join(CORE_DIR, "oscillators.pyx")],
-        include_dirs=[numpy.get_include()],
-        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
-    ),
-    Extension(
-        "trends",
-        [os.path.join(CORE_DIR, "trends.pyx")],
-        include_dirs=[numpy.get_include()],
-        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
-    ),
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    )
+    for name in module_names
 ]
 
 setup(
