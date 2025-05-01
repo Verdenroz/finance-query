a5dac705ada830c221a59655fbf7ae33af9bf49e
harvey <harveytseng2@gmail.com>
2025-05-01 15:38:39 -0400

    tests removing cache

--- src/services/indices/get_indices.py
+++ src/services/indices/get_indices.py
@@ -1,13 +1,11 @@
 import asyncio
 
-from src.cache import cache
 from src.models import MarketIndex
 from src.models.index import Index
 from src.services.indices.fetchers import fetch_index
 from src.services.quotes import get_adaptive_chunk_size
 
 
-@cache(expire=15, market_closed_expire=180)
 async def get_indices(cookies: str, crumb: str, indices: list[Index] = None) -> list[MarketIndex]:
     """
     Gets an aggregated performance of major world market indices or specific indices.
