e3abd12aaadcce71128c77ec9bb0458c823c1f7e
harvey <harveytseng2@gmail.com>
2025-05-13 19:38:17 -0400

    refactors similar retrieval to use FinanceClient, removing cookie and crumb dependencies

--- src/services/similar/get_similar_quotes.py
+++ src/services/similar/get_similar_quotes.py
@@ -1,20 +1,20 @@
-from utils.cache import cache
 from src.models import SimpleQuote
 from src.services.similar.fetchers import fetch_similar, scrape_similar_quotes
+from utils.cache import cache
+from utils.dependencies import FinanceClient
 
 
 @cache(expire=15, market_closed_expire=600)
-async def get_similar_quotes(symbol: str, cookies: dict, crumb: str, limit: int = 10) -> list[SimpleQuote]:
+async def get_similar_quotes(finance_client: FinanceClient, symbol: str, limit: int = 10) -> list[SimpleQuote]:
     """
     Get similar stocks by API or scrape if API fails
-    :param cookies: authentication cookies for Yahoo Finance
-    :param crumb: authentication crumb for Yahoo Finance
+    :param finance_client: The Yahoo Finance client to use for API requests
     :param symbol: the symbol of the stock to find similar stocks around
     :param limit: the number of similar stocks to return
 
     :return: a list of SimpleQuote objects
     """
-    similar = await fetch_similar(symbol, limit, cookies, crumb)
+    similar = await fetch_similar(finance_client, symbol, limit)
 
     if not similar:
         return await scrape_similar_quotes(symbol, limit)
