a895eeca8cc394187c26a021cdac81d036394bc3
harvey <harveytseng2@gmail.com>
2025-11-03 17:56:48 -0500

    fix: fixes indicator test key errors

--- tests/services/indicators/test_ma.py
+++ tests/services/indicators/test_ma.py
@@ -20,9 +20,9 @@ class TestMA:
         assert isinstance(result, dict)
         assert result["type"] == Indicator.SMA
         assert "Technical Analysis" in result
-        assert round(result["Technical Analysis"]["2025-03-14"].value, 2) == 113.93
-        assert round(result["Technical Analysis"]["2025-01-16"].value, 2) == 138.32
-        assert round(result["Technical Analysis"]["2024-09-27"].value, 2) == 118.57
+        assert round(result["Technical Analysis"]["2025-03-14"]["SMA"], 2) == 113.93
+        assert round(result["Technical Analysis"]["2025-01-16"]["SMA"], 2) == 138.32
+        assert round(result["Technical Analysis"]["2024-09-27"]["SMA"], 2) == 118.57
 
     async def test_get_ema(self, historical_quotes, mock_finance_client, monkeypatch):
         """Test the get_ema function with real data from fixture"""
@@ -39,9 +39,9 @@ class TestMA:
         assert isinstance(result, dict)
         assert result["type"] == Indicator.EMA
         assert "Technical Analysis" in result
-        assert round(result["Technical Analysis"]["2025-03-14"].value, 2) == 116.76
-        assert round(result["Technical Analysis"]["2025-01-16"].value, 2) == 136.33
-        assert round(result["Technical Analysis"]["2024-09-27"].value, 2) == 119.40
+        assert round(result["Technical Analysis"]["2025-03-14"]["EMA"], 2) == 116.76
+        assert round(result["Technical Analysis"]["2025-01-16"]["EMA"], 2) == 136.33
+        assert round(result["Technical Analysis"]["2024-09-27"]["EMA"], 2) == 119.40
 
     async def test_get_wma(self, historical_quotes, mock_finance_client, monkeypatch):
         """Test the get_wma function with real data from fixture"""
@@ -58,9 +58,9 @@ class TestMA:
         assert isinstance(result, dict)
         assert result["type"] == Indicator.WMA
         assert "Technical Analysis" in result
-        assert round(result["Technical Analysis"]["2025-03-14"].value, 2) == 113.51
-        assert round(result["Technical Analysis"]["2025-01-16"].value, 2) == 140.26
-        assert round(result["Technical Analysis"]["2024-09-27"].value, 2) == 117.11
+        assert round(result["Technical Analysis"]["2025-03-14"]["WMA"], 2) == 113.51
+        assert round(result["Technical Analysis"]["2025-01-16"]["WMA"], 2) == 140.26
+        assert round(result["Technical Analysis"]["2024-09-27"]["WMA"], 2) == 117.11
 
     async def test_get_vwma(self, historical_quotes, mock_finance_client, monkeypatch):
         """Test the get_vwma function with real data from fixture"""
@@ -77,6 +77,6 @@ class TestMA:
         assert isinstance(result, dict)
         assert result["type"] == Indicator.VWMA
         assert "Technical Analysis" in result
-        assert round(result["Technical Analysis"]["2025-03-14"].value, 2) == 121.0
-        assert round(result["Technical Analysis"]["2025-01-16"].value, 2) == 136.77
-        assert round(result["Technical Analysis"]["2024-09-27"].value, 2) == 114.55
+        assert round(result["Technical Analysis"]["2025-03-14"]["VWMA"], 2) == 121.0
+        assert round(result["Technical Analysis"]["2025-01-16"]["VWMA"], 2) == 136.77
+        assert round(result["Technical Analysis"]["2024-09-27"]["VWMA"], 2) == 114.55
